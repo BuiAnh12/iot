ee9e6a5710b8fe3166df931c2ac71adbac8510e8
Dat Nguyen <=>
2025-03-19 08:40:26 +0700

    Fix bug

--- start_stream.py
+++ start_stream.py
@@ -3,17 +3,23 @@ import time
 import cv2
 import os
 
+FFMPEG_PATH = r"D:/ffmpeg/bin/ffmpeg.exe"
+
 def start_stream(video_path, stream_id, localhost_url):
     command = [
-        "ffmpeg", "-re", "-stream_loop", "-1", "-i", video_path,
-        "-c", "copy", "-f", "rtsp", "-rtsp_transport", "tcp", localhost_url
-    ]
+    FFMPEG_PATH, "-re", "-stream_loop", "-1", "-i", video_path,
+    "-c:v", "libx264", "-preset", "ultrafast", "-tune", "zerolatency",
+    "-b:v", "500k", "-maxrate", "500k", "-bufsize", "1000k",
+    "-c:a", "aac", "-f", "rtsp", "-rtsp_transport", "tcp", localhost_url
+]
     
     try:
-        subprocess.Popen(command)
+        subprocess.Popen(command, shell=True)
         print(f"Streaming started: {localhost_url}")
-    except subprocess.CalledProcessError as e:
-        print(f"Error: {e}")
+    except FileNotFoundError:
+        print("Lỗi: Không tìm thấy FFmpeg. Kiểm tra đường dẫn.")
+    except Exception as e:
+        print(f"Lỗi khác: {e}")
 
 def check_stream(rtsp_url):
     cap = cv2.VideoCapture(rtsp_url)
@@ -26,7 +32,7 @@ def check_stream(rtsp_url):
 
 
 if __name__ == "__main__":
-    video_file = "E:/PTITv2/iot/video/loitering_people.mp4"
+    video_file = "D:/Workspace Embedded/iot/video/1010(1)-1.mp4"
     stream_id = "mystream"
     localhost_url = f"rtsp://localhost:8554/{stream_id}"
     
